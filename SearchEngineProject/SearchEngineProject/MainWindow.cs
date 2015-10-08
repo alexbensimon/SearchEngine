e63259940acd806bccf78f0198a388ff29e952f8
Vincent Gagneux <vincentgagneux@outlook.com>
2015-10-08 05:06:57 -0700

    zblah

--- SearchEngineProject/SearchEngineProject/MainWindow.cs
+++ SearchEngineProject/SearchEngineProject/MainWindow.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using SearchEngineProject.Properties;
